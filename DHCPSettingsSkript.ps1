78c176b4be234579846c823ee3420decd84e50d7
Philip Marti <philip_marti@besonet.ch>
2017-10-27 16:21:11 +0200

    made some small changes to the script

--- DHCPSettingsSkript.ps1
+++ DHCPSettingsSkript.ps1
@@ -1,6 +1,6 @@
 ###################################################################
 ###################################################################
-### This module is thought to determine if DHCP works correctly ###
+### This script is thought to determine if DHCP works correctly ###
 ### author: Corporate Software, Philip Marti                    ###
 ###################################################################
 ###################################################################
@@ -11,8 +11,8 @@ function Get-DHCPStatus {
     function Get-Adapter {
         [array]$AdapterName = $null
         Get-NetAdapter | ForEach-Object {
-            if (($_).Status -eq "Up" -and (($_).Name -eq "Ethernet" -or ($_).Name -eq "Wi-Fi" -or ($_).Name -eq "WLAN")) {
-                [array]$AdapterName += ($_).Name
+            if ($_.Status -eq "Up" -and ($_.Name -eq "Ethernet" -or $_.Name -eq "Wi-Fi" -or $_.Name -eq "WLAN")) {
+                [array]$AdapterName += $_.Name
             }
         }
         return $AdapterName
@@ -20,16 +20,18 @@ function Get-DHCPStatus {
 
     # Get the InterfaceAliases, IPv4-Addresses and subnet masks, which were applied by dhcp
     function Get-IPAddressCIDR {
+        $adapter = Get-Adapter
         [array]$Interface_IPAddress_Subnetmask = $null # objects with properties [InterfaceAlias, IPv4Address, Subnetmask (CIDR-Notation)]
         [array]$wishedInterface = $null # All interfaces which received their Lease from DHCP and their IPAddressFamily is not IPv6
         $IPObject = $null # Element of the Interface_IPAddress_Subnetmask Array
 
-        $wishedInterface = Get-NetIPAddress | Where-Object {($_).PrefixOrigin -eq "Dhcp" -and ($_).AddressFamily -ne "IPv6"}
+        $wishedInterface = Get-NetIPAddress -InterfaceAlias $adapter | Where-Object {$_.PrefixOrigin -eq "Dhcp"}
         foreach ($interface in $wishedInterface) {
             $IPObject = [PSCustomObject]@{
-                InterfaceAlias = ($interface).InterfaceAlias
-                IPAddress      = ($interface).IPAddress
-                PrefixLength   = ($interface).PrefixLength
+                InterfaceAlias = $interface.InterfaceAlias
+                IPAddress      = $interface.IPAddress
+                PrefixLength   = $interface.PrefixLength
+                ValidLifetime  = $interface.ValidLifetime
             }
             [array]$Interface_IPAddress_Subnetmask += $IPObject
         }
@@ -42,8 +44,8 @@ function Get-DHCPStatus {
         $gateway = $null
         Get-NetIPConfiguration | ForEach-Object {
             $gateway = [PSCustomObject]@{
-                InterfaceAlias = (($_).IPv4DefaultGateway).InterfaceAlias
-                DefaultGateway = (($_).IPv4DefaultGateway).NextHop
+                InterfaceAlias = ($_.IPv4DefaultGateway).InterfaceAlias
+                DefaultGateway = ($_.IPv4DefaultGateway).NextHop
             }
             [array]$Interface_DefaultGateway += $gateway
         }
@@ -56,8 +58,8 @@ function Get-DHCPStatus {
         $DNSInterfaces = Get-DnsClientServerAddress
 
         foreach ($interface in $DNSInterfaces) {
-            if (($interface).ServerAddresses -like "*.*") {
-                $address = (($interface).ServerAddresses).trim("{", "}")
+            if ($interface.ServerAddresses -like "*.*") {
+                $address = ($interface.ServerAddresses).trim("{", "}")
             }
             else {
                 $address = ""
@@ -65,7 +67,7 @@ function Get-DHCPStatus {
 
             if ($address -ne "") {
                 $DNSObject = [PSCustomObject]@{
-                    InterfaceAlias = ($interface).InterfaceAlias
+                    InterfaceAlias = $interface.InterfaceAlias
                     IPv4Address    = $address
                 }
                 $Interface_DNS += $DNSObject
@@ -90,6 +92,7 @@ function Get-DHCPStatus {
         $InterfaceAlias = $a
         $ip = $null
         $netmask = $null
+        $lease = $null
         $gateway = $null
         $dns = $null
         $release = $null
@@ -99,21 +102,22 @@ function Get-DHCPStatus {
         # Make some calculations and assignments
         
         foreach ($ipobj in $IPObjects) {
-            if (($ipobj).InterfaceAlias -eq $a) {
-                $ip = ($ipobj).IPAddress
-                $netmask = ($ipobj).PrefixLength
+            if ($ipobj.InterfaceAlias -eq $a) {
+                $ip = $ipobj.IPAddress
+                $netmask = $ipobj.PrefixLength
+                $lease = $ipobj.ValidLifetime
             }
         }
 
         foreach ($gw in $GatewayObjects) {
-            if (($gw).InterfaceAlias -eq $a) {
-                $gateway = ($gw).DefaultGateway
+            if ($gw.InterfaceAlias -eq $a) {
+                $gateway = $gw.DefaultGateway
             }
         }
 
         foreach ($dnsobj in $DNSObjects) {
-            if (($dnsobj).InterfaceAlias -eq $a) {
-                $dns = ($dnsobj).IPv4Address
+            if ($dnsobj.InterfaceAlias -eq $a) {
+                $dns = $dnsobj.IPv4Address
             }
         }
 
@@ -121,8 +125,8 @@ function Get-DHCPStatus {
         ipconfig.exe /release $a > $null
         $IPObjects_2 = Get-IPAddressCIDR
         foreach ($ipobj in $IPObjects_2) {
-            if (($ipobj).InterfaceAlias -eq $a) {
-                $ip_a = ($ipobj).IPAddress
+            if ($ipobj.InterfaceAlias -eq $a) {
+                $ip_a = $ipobj.IPAddress
             }
         } 
         if ($ip_a -eq "" -or $ip_a -like "169.254.*" -or $ip_a -eq $null) {
@@ -137,8 +141,8 @@ function Get-DHCPStatus {
         ipconfig.exe /renew $a > $null
         $IPObjects_3 = Get-IPAddressCIDR
         foreach ($ipobj in $IPObjects_3) {
-            if (($ipobj).InterfaceAlias -eq $a) {
-                $ip_a = ($ipobj).IPAddress
+            if ($ipobj.InterfaceAlias -eq $a) {
+                $ip_a = $ipobj.IPAddress
             }
         } 
         if ($ip_a -eq "" -or $ip_a -like "169.254.*" -or $ip_a -eq $null) {
@@ -161,6 +165,7 @@ function Get-DHCPStatus {
                 InterfaceAlias  = $InterfaceAlias
                 IPAddress       = $ip
                 PrefixLength    = $netmask
+                ValidLifetime   = $lease
                 DefaultGateway  = $gateway
                 DNSServer       = $dns
                 ReleasePossible = $release
