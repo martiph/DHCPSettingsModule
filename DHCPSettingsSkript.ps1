a8443a47ff50220e0511ae987dc8e91c2d227c10
Philip Marti <philip_marti@besonet.ch>
2017-10-27 10:53:23 +0200

    formatted the code with vscode

--- DHCPSettingsSkript.ps1
+++ DHCPSettingsSkript.ps1
@@ -5,31 +5,31 @@
 ###################################################################
 ###################################################################
 
-function Get-DHCPStatus{
+function Get-DHCPStatus {
     
     # Get all running adapters which are in ["Ethernet", "Wi-Fi", "WLAN"]
-    function Get-Adapter{
+    function Get-Adapter {
         [array]$AdapterName = $null
         Get-NetAdapter | ForEach-Object {
-            if(($_).Status -eq "Up" -and (($_).Name -eq "Ethernet" -or ($_).Name -eq "Wi-Fi" -or ($_).Name -eq "WLAN")){
-                [array]$AdapterName +=  ($_).Name
+            if (($_).Status -eq "Up" -and (($_).Name -eq "Ethernet" -or ($_).Name -eq "Wi-Fi" -or ($_).Name -eq "WLAN")) {
+                [array]$AdapterName += ($_).Name
             }
         }
         return $AdapterName
     }
 
     # Get the InterfaceAliases, IPv4-Addresses and subnet masks, which were applied by dhcp
-    function Get-IPAddressCIDR{
+    function Get-IPAddressCIDR {
         [array]$Interface_IPAddress_Subnetmask = $null # objects with properties [InterfaceAlias, IPv4Address, Subnetmask (CIDR-Notation)]
         [array]$wishedInterface = $null # All interfaces which received their Lease from DHCP and their IPAddressFamily is not IPv6
         $IPObject = $null # Element of the Interface_IPAddress_Subnetmask Array
 
-        $wishedInterface = Get-NetIPAddress | Where-Object{($_).PrefixOrigin -eq "Dhcp" -and ($_).AddressFamily -ne "IPv6"}
-        foreach($interface in $wishedInterface){
+        $wishedInterface = Get-NetIPAddress | Where-Object {($_).PrefixOrigin -eq "Dhcp" -and ($_).AddressFamily -ne "IPv6"}
+        foreach ($interface in $wishedInterface) {
             $IPObject = [PSCustomObject]@{
                 InterfaceAlias = ($interface).InterfaceAlias
-                IPAddress = ($interface).IPAddress
-                PrefixLength = ($interface).PrefixLength
+                IPAddress      = ($interface).IPAddress
+                PrefixLength   = ($interface).PrefixLength
             }
             [array]$Interface_IPAddress_Subnetmask += $IPObject
         }
@@ -37,10 +37,10 @@ function Get-DHCPStatus{
     }
 
     # Get the pair DefaultGateway, InterfaceAlias
-    function Get-DefaultGateway{
+    function Get-DefaultGateway {
         [array]$Interface_DefaultGateway = $null
         $gateway = $null
-        Get-NetIPConfiguration | ForEach-Object{
+        Get-NetIPConfiguration | ForEach-Object {
             $gateway = [PSCustomObject]@{
                 InterfaceAlias = (($_).IPv4DefaultGateway).InterfaceAlias
                 DefaultGateway = (($_).IPv4DefaultGateway).NextHop
@@ -51,21 +51,22 @@ function Get-DHCPStatus{
     }
 
     # Get the pair DNSServer, InterfaceAlias
-    function Get-DNSServer{
+    function Get-DNSServer {
         [array]$Interface_DNS = $null
         $DNSInterfaces = Get-DnsClientServerAddress
 
-        foreach($interface in $DNSInterfaces){
-            if(($interface).ServerAddresses -like "*.*"){
+        foreach ($interface in $DNSInterfaces) {
+            if (($interface).ServerAddresses -like "*.*") {
                 $address = (($interface).ServerAddresses).trim("{", "}")
-            }else{
+            }
+            else {
                 $address = ""
             }
 
-            if($address -ne ""){
+            if ($address -ne "") {
                 $DNSObject = [PSCustomObject]@{
                     InterfaceAlias = ($interface).InterfaceAlias
-                    IPv4Address = $address
+                    IPv4Address    = $address
                 }
                 $Interface_DNS += $DNSObject
             }
@@ -83,7 +84,7 @@ function Get-DHCPStatus{
     $GatewayObjects = Get-DefaultGateway
     $DNSObjects = Get-DNSServer
 
-    foreach($a in $Adapter){
+    foreach ($a in $Adapter) {
         
         # initialize some variables
         $InterfaceAlias = $a
@@ -97,21 +98,21 @@ function Get-DHCPStatus{
 
         # Make some calculations and assignments
         
-        foreach($ipobj in $IPObjects){
-            if(($ipobj).InterfaceAlias -eq $a){
+        foreach ($ipobj in $IPObjects) {
+            if (($ipobj).InterfaceAlias -eq $a) {
                 $ip = ($ipobj).IPAddress
-                $netmask =  ($ipobj).PrefixLength
+                $netmask = ($ipobj).PrefixLength
             }
         }
 
-        foreach($gw in $GatewayObjects){
-            if(($gw).InterfaceAlias -eq $a){
+        foreach ($gw in $GatewayObjects) {
+            if (($gw).InterfaceAlias -eq $a) {
                 $gateway = ($gw).DefaultGateway
             }
         }
 
-        foreach($dnsobj in $DNSObjects){
-            if(($dnsobj).InterfaceAlias -eq $a){
+        foreach ($dnsobj in $DNSObjects) {
+            if (($dnsobj).InterfaceAlias -eq $a) {
                 $dns = ($dnsobj).IPv4Address
             }
         }
@@ -119,14 +120,15 @@ function Get-DHCPStatus{
         # check if the release of the ip address is working
         ipconfig.exe /release $a > $null
         $IPObjects_2 = Get-IPAddressCIDR
-        foreach($ipobj in $IPObjects_2){
-            if(($ipobj).InterfaceAlias -eq $a){
+        foreach ($ipobj in $IPObjects_2) {
+            if (($ipobj).InterfaceAlias -eq $a) {
                 $ip_a = ($ipobj).IPAddress
             }
         } 
         if ($ip_a -eq "" -or $ip_a -like "169.254.*" -or $ip_a -eq $null) {
             $release = $true
-        }else{
+        }
+        else {
             $release = $false
         }
 
@@ -134,14 +136,15 @@ function Get-DHCPStatus{
         $ip_a = $null
         ipconfig.exe /renew $a > $null
         $IPObjects_3 = Get-IPAddressCIDR
-        foreach($ipobj in $IPObjects_3){
-            if(($ipobj).InterfaceAlias -eq $a){
+        foreach ($ipobj in $IPObjects_3) {
+            if (($ipobj).InterfaceAlias -eq $a) {
                 $ip_a = ($ipobj).IPAddress
             }
         } 
         if ($ip_a -eq "" -or $ip_a -like "169.254.*" -or $ip_a -eq $null) {
             $renew = $false
-        }else{
+        }
+        else {
             $renew = $true
         }
 
@@ -153,15 +156,15 @@ function Get-DHCPStatus{
         #       Ipconfig /release und Ipconfig /renew sollten niemals unbedacht verwendet werden, da damit die Verbindung unterbrochen wird.      #
 
         #create the object
-        if($ip -ne $null -and $netmask -ne $null){
+        if ($ip -ne $null -and $netmask -ne $null) {
             $output += [PSCustomObject]@{
-                InterfaceAlias = $InterfaceAlias
-                IPAddress = $ip
-                PrefixLength = $netmask
-                DefaultGateway = $gateway
-                DNSServer = $dns
+                InterfaceAlias  = $InterfaceAlias
+                IPAddress       = $ip
+                PrefixLength    = $netmask
+                DefaultGateway  = $gateway
+                DNSServer       = $dns
                 ReleasePossible = $release
-                RenewPossible = $renew
+                RenewPossible   = $renew
             }
         }
     }
