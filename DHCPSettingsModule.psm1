fa9bd408b86527d75887c029c82192c2e0503330
Philip Marti <philip_marti@besonet.ch>
2017-11-01 12:56:29 +0100

    created a ModuleManifest and made some refactoring with the script and module

--- DHCPSettingsModule.psm1
+++ DHCPSettingsModule.psm1
@@ -6,77 +6,6 @@
 ###################################################################
 
 function Get-DHCPStatus {
-    
-    # Get all running adapters which are in ["Ethernet", "Wi-Fi", "WLAN"]
-    function Get-Adapter {
-        [array]$AdapterName = $null
-        Get-NetAdapter | ForEach-Object {
-            if (($_).Status -eq "Up" -and (($_).Name -eq "Ethernet" -or ($_).Name -eq "Wi-Fi" -or ($_).Name -eq "WLAN")) {
-                [array]$AdapterName += ($_).Name
-            }
-        }
-        return $AdapterName
-    }
-
-    # Get the InterfaceAliases, IPv4-Addresses and subnet masks, which were applied by dhcp
-    function Get-IPAddressCIDR {
-        [array]$Interface_IPAddress_Subnetmask = $null # objects with properties [InterfaceAlias, IPv4Address, Subnetmask (CIDR-Notation)]
-        [array]$wishedInterface = $null # All interfaces which received their Lease from DHCP and their IPAddressFamily is not IPv6
-        $IPObject = $null # Element of the Interface_IPAddress_Subnetmask Array
-
-        $wishedInterface = Get-NetIPAddress | Where-Object {($_).PrefixOrigin -eq "Dhcp" -and ($_).AddressFamily -ne "IPv6"}
-        foreach ($interface in $wishedInterface) {
-            $IPObject = [PSCustomObject]@{
-                InterfaceAlias = ($interface).InterfaceAlias
-                IPAddress      = ($interface).IPAddress
-                PrefixLength   = ($interface).PrefixLength
-            }
-            [array]$Interface_IPAddress_Subnetmask += $IPObject
-        }
-        return $Interface_IPAddress_Subnetmask
-    }
-
-    # Get the pair DefaultGateway, InterfaceAlias
-    function Get-DefaultGateway {
-        [array]$Interface_DefaultGateway = $null
-        $gateway = $null
-        Get-NetIPConfiguration | ForEach-Object {
-            $gateway = [PSCustomObject]@{
-                InterfaceAlias = (($_).IPv4DefaultGateway).InterfaceAlias
-                DefaultGateway = (($_).IPv4DefaultGateway).NextHop
-            }
-            [array]$Interface_DefaultGateway += $gateway
-        }
-        return $Interface_DefaultGateway
-    }
-
-    # Get the pair DNSServer, InterfaceAlias
-    function Get-DNSServer {
-        [array]$Interface_DNS = $null
-        $DNSInterfaces = Get-DnsClientServerAddress
-
-        foreach ($interface in $DNSInterfaces) {
-            if (($interface).ServerAddresses -like "*.*") {
-                $address = (($interface).ServerAddresses).trim("{", "}")
-            }
-            else {
-                $address = ""
-            }
-
-            if ($address -ne "") {
-                $DNSObject = [PSCustomObject]@{
-                    InterfaceAlias = ($interface).InterfaceAlias
-                    IPv4Address    = $address
-                }
-                $Interface_DNS += $DNSObject
-            }
-        }
-        return $Interface_DNS
-    }
-
-    ##########################################
-    ################# OUTPUT #################
-    ##########################################
 
     # Create the output-object
     $Adapter = Get-Adapter
@@ -90,6 +19,7 @@ function Get-DHCPStatus {
         $InterfaceAlias = $a
         $ip = $null
         $netmask = $null
+        $lease = $null
         $gateway = $null
         $dns = $null
         $release = $null
@@ -99,21 +29,22 @@ function Get-DHCPStatus {
         # Make some calculations and assignments
         
         foreach ($ipobj in $IPObjects) {
-            if (($ipobj).InterfaceAlias -eq $a) {
-                $ip = ($ipobj).IPAddress
-                $netmask = ($ipobj).PrefixLength
+            if ($ipobj.InterfaceAlias -eq $a) {
+                $ip = $ipobj.IPAddress
+                $netmask = $ipobj.PrefixLength
+                $lease = $ipobj.ValidLifetime
             }
         }
 
         foreach ($gw in $GatewayObjects) {
-            if (($gw).InterfaceAlias -eq $a) {
-                $gateway = ($gw).DefaultGateway
+            if ($gw.InterfaceAlias -eq $a) {
+                $gateway = $gw.DefaultGateway
             }
         }
 
         foreach ($dnsobj in $DNSObjects) {
-            if (($dnsobj).InterfaceAlias -eq $a) {
-                $dns = ($dnsobj).IPv4Address
+            if ($dnsobj.InterfaceAlias -eq $a) {
+                $dns = $dnsobj.IPv4Address
             }
         }
 
@@ -121,8 +52,8 @@ function Get-DHCPStatus {
         ipconfig.exe /release $a > $null
         $IPObjects_2 = Get-IPAddressCIDR
         foreach ($ipobj in $IPObjects_2) {
-            if (($ipobj).InterfaceAlias -eq $a) {
-                $ip_a = ($ipobj).IPAddress
+            if ($ipobj.InterfaceAlias -eq $a) {
+                $ip_a = $ipobj.IPAddress
             }
         } 
         if ($ip_a -eq "" -or $ip_a -like "169.254.*" -or $ip_a -eq $null) {
@@ -137,8 +68,8 @@ function Get-DHCPStatus {
         ipconfig.exe /renew $a > $null
         $IPObjects_3 = Get-IPAddressCIDR
         foreach ($ipobj in $IPObjects_3) {
-            if (($ipobj).InterfaceAlias -eq $a) {
-                $ip_a = ($ipobj).IPAddress
+            if ($ipobj.InterfaceAlias -eq $a) {
+                $ip_a = $ipobj.IPAddress
             }
         } 
         if ($ip_a -eq "" -or $ip_a -like "169.254.*" -or $ip_a -eq $null) {
@@ -148,19 +79,13 @@ function Get-DHCPStatus {
             $renew = $true
         }
 
-        ### Hier kommt noch die Implementierung für die folgende Überprüfung:
-        ### Funktioniert ein renes? --> ipconfig /renew <Adaptername> könnte funktionieren
-        ### Die grundlegende Idee dabei sollte sein, dass zuerst alle DHCP-Parameter abgefragt werden, ein 
-        ### Release/Renew durchgeführt wird und überprüft wird, ob die IP-Adresse zwischenzeitliche eine APIPA-Adresse war
-        ### und ob die jetzige wieder eine gültige IPv4 Adresse ist.
-        #       Ipconfig /release und Ipconfig /renew sollten niemals unbedacht verwendet werden, da damit die Verbindung unterbrochen wird.      #
-
         #create the object
         if ($ip -ne $null -and $netmask -ne $null) {
             $output += [PSCustomObject]@{
                 InterfaceAlias  = $InterfaceAlias
-                IPAddress       = $ip
+                IpAddress       = $ip
                 PrefixLength    = $netmask
+                ValidLifetime   = $lease
                 DefaultGateway  = $gateway
                 DNSServer       = $dns
                 ReleasePossible = $release
@@ -169,4 +94,75 @@ function Get-DHCPStatus {
         }
     }
     return $output
+}
+
+### Helping Functions ###
+
+# Get all running adapters which are in ["Ethernet", "Wi-Fi", "WLAN"]
+function Get-Adapter {
+    [array]$AdapterName = $null
+    Get-NetAdapter | ForEach-Object {
+        if ($_.Status -eq "Up" -and ($_.Name -eq "Ethernet" -or $_.Name -eq "Wi-Fi" -or $_.Name -eq "WLAN")) {
+            [array]$AdapterName += $_.Name
+        }
+    }
+    return $AdapterName
+}
+
+# Get the InterfaceAliases, IPv4-Addresses and subnet masks, which were applied by dhcp
+function Get-IPAddressCIDR {
+    $adapter = Get-Adapter
+    [array]$Interface_IPAddress_Subnetmask = $null # objects with properties [InterfaceAlias, IPv4Address, Subnetmask (CIDR-Notation)]
+    [array]$wishedInterface = $null # All interfaces which received their Lease from DHCP and their IPAddressFamily is not IPv6
+    $IPObject = $null # Element of the Interface_IPAddress_Subnetmask Array
+
+    $wishedInterface = Get-NetIPAddress -InterfaceAlias $adapter| Where-Object {$_.PrefixOrigin -eq "Dhcp"}
+    foreach ($interface in $wishedInterface) {
+        $IPObject = [PSCustomObject]@{
+            InterfaceAlias = $interface.InterfaceAlias
+            IPAddress      = $interface.IPAddress
+            PrefixLength   = $interface.PrefixLength
+            ValidLifetime  = $interface.ValidLifetime
+        }
+        [array]$Interface_IPAddress_Subnetmask += $IPObject
+    }
+    return $Interface_IPAddress_Subnetmask
+}
+
+# Get the pair DefaultGateway, InterfaceAlias
+function Get-DefaultGateway {
+    [array]$Interface_DefaultGateway = $null
+    $gateway = $null
+    Get-NetIPConfiguration | ForEach-Object {
+        $gateway = [PSCustomObject]@{
+            InterfaceAlias = ($_.IPv4DefaultGateway).InterfaceAlias
+            DefaultGateway = ($_.IPv4DefaultGateway).NextHop
+        }
+        [array]$Interface_DefaultGateway += $gateway
+    }
+    return $Interface_DefaultGateway
+}
+
+# Get the pair DNSServer, InterfaceAlias
+function Get-DNSServer {
+    [array]$Interface_DNS = $null
+    $DNSInterfaces = Get-DnsClientServerAddress
+
+    foreach ($interface in $DNSInterfaces) {
+        if ($interface.ServerAddresses -like "*.*") {
+            $address = ($interface.ServerAddresses).trim("{", "}")
+        }
+        else {
+            $address = ""
+        }
+
+        if ($address -ne "") {
+            $DNSObject = [PSCustomObject]@{
+                InterfaceAlias = ($interface).InterfaceAlias
+                IPv4Address    = $address
+            }
+            $Interface_DNS += $DNSObject
+        }
+    }
+    return $Interface_DNS
 }
--- DHCPSettingsSkript.ps1
+++ DHCPSettingsSkript.ps1
@@ -6,79 +6,6 @@
 ###################################################################
 
 function Get-DHCPStatus {
-    
-    # Get all running adapters which are in ["Ethernet", "Wi-Fi", "WLAN"]
-    function Get-Adapter {
-        [array]$AdapterName = $null
-        Get-NetAdapter | ForEach-Object {
-            if ($_.Status -eq "Up" -and ($_.Name -eq "Ethernet" -or $_.Name -eq "Wi-Fi" -or $_.Name -eq "WLAN")) {
-                [array]$AdapterName += $_.Name
-            }
-        }
-        return $AdapterName
-    }
-
-    # Get the InterfaceAliases, IPv4-Addresses and subnet masks, which were applied by dhcp
-    function Get-IPAddressCIDR {
-        $adapter = Get-Adapter
-        [array]$Interface_IPAddress_Subnetmask = $null # objects with properties [InterfaceAlias, IPv4Address, Subnetmask (CIDR-Notation)]
-        [array]$wishedInterface = $null # All interfaces which received their Lease from DHCP and their IPAddressFamily is not IPv6
-        $IPObject = $null # Element of the Interface_IPAddress_Subnetmask Array
-
-        $wishedInterface = Get-NetIPAddress -InterfaceAlias $adapter | Where-Object {$_.PrefixOrigin -eq "Dhcp"}
-        foreach ($interface in $wishedInterface) {
-            $IPObject = [PSCustomObject]@{
-                InterfaceAlias = $interface.InterfaceAlias
-                IPAddress      = $interface.IPAddress
-                PrefixLength   = $interface.PrefixLength
-                ValidLifetime  = $interface.ValidLifetime
-            }
-            [array]$Interface_IPAddress_Subnetmask += $IPObject
-        }
-        return $Interface_IPAddress_Subnetmask
-    }
-
-    # Get the pair DefaultGateway, InterfaceAlias
-    function Get-DefaultGateway {
-        [array]$Interface_DefaultGateway = $null
-        $gateway = $null
-        Get-NetIPConfiguration | ForEach-Object {
-            $gateway = [PSCustomObject]@{
-                InterfaceAlias = ($_.IPv4DefaultGateway).InterfaceAlias
-                DefaultGateway = ($_.IPv4DefaultGateway).NextHop
-            }
-            [array]$Interface_DefaultGateway += $gateway
-        }
-        return $Interface_DefaultGateway
-    }
-
-    # Get the pair DNSServer, InterfaceAlias
-    function Get-DNSServer {
-        [array]$Interface_DNS = $null
-        $DNSInterfaces = Get-DnsClientServerAddress
-
-        foreach ($interface in $DNSInterfaces) {
-            if ($interface.ServerAddresses -like "*.*") {
-                $address = ($interface.ServerAddresses).trim("{", "}")
-            }
-            else {
-                $address = ""
-            }
-
-            if ($address -ne "") {
-                $DNSObject = [PSCustomObject]@{
-                    InterfaceAlias = $interface.InterfaceAlias
-                    IPv4Address    = $address
-                }
-                $Interface_DNS += $DNSObject
-            }
-        }
-        return $Interface_DNS
-    }
-
-    ##########################################
-    ################# OUTPUT #################
-    ##########################################
 
     # Create the output-object
     $Adapter = Get-Adapter
@@ -152,18 +79,11 @@ function Get-DHCPStatus {
             $renew = $true
         }
 
-        ### Hier kommt noch die Implementierung für die folgende Überprüfung:
-        ### Funktioniert ein renes? --> ipconfig /renew <Adaptername> könnte funktionieren
-        ### Die grundlegende Idee dabei sollte sein, dass zuerst alle DHCP-Parameter abgefragt werden, ein 
-        ### Release/Renew durchgeführt wird und überprüft wird, ob die IP-Adresse zwischenzeitliche eine APIPA-Adresse war
-        ### und ob die jetzige wieder eine gültige IPv4 Adresse ist.
-        #       Ipconfig /release und Ipconfig /renew sollten niemals unbedacht verwendet werden, da damit die Verbindung unterbrochen wird.      #
-
         #create the object
         if ($ip -ne $null -and $netmask -ne $null) {
             $output += [PSCustomObject]@{
                 InterfaceAlias  = $InterfaceAlias
-                IPAddress       = $ip
+                IpAddress       = $ip
                 PrefixLength    = $netmask
                 ValidLifetime   = $lease
                 DefaultGateway  = $gateway
@@ -174,4 +94,75 @@ function Get-DHCPStatus {
         }
     }
     return $output
+}
+
+### Helping Functions ###
+
+# Get all running adapters which are in ["Ethernet", "Wi-Fi", "WLAN"]
+function Get-Adapter {
+    [array]$AdapterName = $null
+    Get-NetAdapter | ForEach-Object {
+        if ($_.Status -eq "Up" -and ($_.Name -eq "Ethernet" -or $_.Name -eq "Wi-Fi" -or $_.Name -eq "WLAN")) {
+            [array]$AdapterName += $_.Name
+        }
+    }
+    return $AdapterName
+}
+
+# Get the InterfaceAliases, IPv4-Addresses and subnet masks, which were applied by dhcp
+function Get-IPAddressCIDR {
+    $adapter = Get-Adapter
+    [array]$Interface_IPAddress_Subnetmask = $null # objects with properties [InterfaceAlias, IPv4Address, Subnetmask (CIDR-Notation)]
+    [array]$wishedInterface = $null # All interfaces which received their Lease from DHCP and their IPAddressFamily is not IPv6
+    $IPObject = $null # Element of the Interface_IPAddress_Subnetmask Array
+
+    $wishedInterface = Get-NetIPAddress -InterfaceAlias $adapter| Where-Object {$_.PrefixOrigin -eq "Dhcp"}
+    foreach ($interface in $wishedInterface) {
+        $IPObject = [PSCustomObject]@{
+            InterfaceAlias = $interface.InterfaceAlias
+            IPAddress      = $interface.IPAddress
+            PrefixLength   = $interface.PrefixLength
+            ValidLifetime  = $interface.ValidLifetime
+        }
+        [array]$Interface_IPAddress_Subnetmask += $IPObject
+    }
+    return $Interface_IPAddress_Subnetmask
+}
+
+# Get the pair DefaultGateway, InterfaceAlias
+function Get-DefaultGateway {
+    [array]$Interface_DefaultGateway = $null
+    $gateway = $null
+    Get-NetIPConfiguration | ForEach-Object {
+        $gateway = [PSCustomObject]@{
+            InterfaceAlias = ($_.IPv4DefaultGateway).InterfaceAlias
+            DefaultGateway = ($_.IPv4DefaultGateway).NextHop
+        }
+        [array]$Interface_DefaultGateway += $gateway
+    }
+    return $Interface_DefaultGateway
+}
+
+# Get the pair DNSServer, InterfaceAlias
+function Get-DNSServer {
+    [array]$Interface_DNS = $null
+    $DNSInterfaces = Get-DnsClientServerAddress
+
+    foreach ($interface in $DNSInterfaces) {
+        if ($interface.ServerAddresses -like "*.*") {
+            $address = ($interface.ServerAddresses).trim("{", "}")
+        }
+        else {
+            $address = ""
+        }
+
+        if ($address -ne "") {
+            $DNSObject = [PSCustomObject]@{
+                InterfaceAlias = ($interface).InterfaceAlias
+                IPv4Address    = $address
+            }
+            $Interface_DNS += $DNSObject
+        }
+    }
+    return $Interface_DNS
 }
